9d131c843c4916cc69f17bf1965994d59628ec02
cc-alejandro-ospina <alejandro.ospina@finoservices.com>
2024-04-25 16:59:42 -0500

    session 9 (interfaces)

--- app.ts
+++ app.ts
@@ -1,30 +1,42 @@
 // Crear interfaces
 
-// Cree una interfaz para validar el auto (el valor enviado por parametro)
-const conducirBatimovil = ( auto ):void => {
-  auto.encender = true;
-  auto.velocidadMaxima = 100;
-  auto.acelear();
+interface Auto {
+  encender: boolean
+  velocidadMaxima: number
+  acelerar(): void
 }
 
-const batimovil = {
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+const conducirBatimovil = ( auto: Auto ):void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelerar();
+}
+
+const batimovil: Auto = {
   encender:false,
   velocidadMaxima:0,
-  acelear(){
+  acelerar(){
     console.log("...... gogogo!!!");
   }
 }
 
 // Cree una interfaz con que permita utilzar el siguiente objeto
 // utilizando propiedades opcionales
+interface Emociones{
+  reir?: boolean
+  comer?: boolean
+  llorar?: boolean
+}
 
-const guason = {
+const guason: Emociones = {
   reir: true,
   comer:true,
   llorar:false
 }
 
-const reir = ( guason ):void => {
+const reir = ( guason:Emociones ):void => {
   if( guason.reir ){
     console.log("JAJAJAJA");
   }
@@ -33,7 +45,11 @@ const reir = ( guason ):void => {
 
 // Cree una interfaz para la siguiente funcion
 
-const ciudadGotica = ( ciudadanos:string[] ):number => {
+interface Ciudadanos {
+  (ciudadanos: string[]): number
+}
+
+const ciudadGotica: Ciudadanos = ( ciudadanos:string[] ):number => {
   return ciudadanos.length;
 }
 
@@ -48,4 +64,21 @@ const ciudadGotica = ( ciudadanos:string[] ):number => {
     - estadoCivil
     - imprimirBio(): void // en consola una breve descripcion.
 */
-class Persona {}
+
+interface data {
+  nombre: string
+  edad: number
+  sexo: string
+  estadoCivil: string
+  imprimirBio(): void
+}
+
+class Persona implements data {
+  public nombre: string
+  public edad: number
+  public sexo: string
+  public estadoCivil: string
+  imprimirBio (){}
+
+
+  }
